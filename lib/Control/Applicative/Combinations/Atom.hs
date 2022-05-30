9b5002c848e4a49172c4cb5dcfad3b2bb4e68582
Barend Venter <barendventer@gmail.com>
2022-05-29 19:24:31 -0700

    Use formatter and explicit imports

--- lib/Control/Applicative/Combinations/Atom.hs
+++ lib/Control/Applicative/Combinations/Atom.hs
@@ -1,9 +1,10 @@
-module Control.Applicative.Combinations.Atom(Atom(..)) where
+module Control.Applicative.Combinations.Atom (Atom (..)) where
+
+import Control.Applicative.Combinations.Class ( Combinations(..) )
 import Data.Dynamic (Typeable)
 import GHC.Generics (Generic)
-import Control.Applicative.Combinations.Class
 
 -- | Indicates that combinations method is to treat this as a single field
-newtype Atom a = Atom { getAtom :: a } deriving (Eq, Show, Read, Ord, Typeable, Generic)
+newtype Atom a = Atom {getAtom :: a} deriving (Eq, Show, Read, Ord, Typeable, Generic)
 
 instance (Applicative f) => Combinations f (Atom a) where combinations = id
